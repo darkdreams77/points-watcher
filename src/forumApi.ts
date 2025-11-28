258aa36db85b8acf5d855ed267ccfd9eb36a4016
darkdreams77 <darkdreams77@users.noreply.github.com>
2025-11-28 23:47:03 +0100

    fix(app): Protect database

--- src/forumApi.ts
+++ src/forumApi.ts
@@ -146,7 +146,7 @@ export async function fetchGroupMembersFromForum(
 
     if (membersOnPage.length === 0) {
       console.warn(`⚠️ AUCUN membre détecté sur ${url} (status ${res.status})`);
-      console.warn('Extrait HTML:', res.data.slice(0, 500));
+      console.warn('Extrait HTML:', res.data.slice(0, 1500));
     }
 
     // === Si aucune entrée trouvée → page vide → on s'arrête ===
