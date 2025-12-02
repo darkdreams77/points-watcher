da14f0840bee7cc4ea0c882f17401ab3548794bc
darkdreams77 <darkdreams77@users.noreply.github.com>
2025-12-02 01:03:25 +0100

    fix: hours

--- frontend/src/helpers/formatDate.ts
+++ frontend/src/helpers/formatDate.ts
@@ -3,7 +3,7 @@ import { DateTime } from 'luxon';
 export function formatDateParis(value: string | null): string {
   if (!value) return '-';
 
-  return DateTime.fromISO(value) // lit le timestamp stocké en DB (UTC)
+  return DateTime.fromSQL(value) // lit le timestamp stocké en DB (UTC)
     .setZone('Europe/Paris') // convertit en heure française
     .toFormat('dd LLL yyyy'); // format lisible (ex: 30 nov 2025)
 }
