9f1a6ee02160b2fc608195cfed22f6fa94fcf1a5
darkdreams77 <darkdreams77@users.noreply.github.com>
2026-08-08 15:39:42 +0200

    fix: manual optimizations

--- frontend/src/helpers/dangerCopyText.ts
+++ frontend/src/helpers/dangerCopyText.ts
@@ -7,10 +7,8 @@ export function buildDangerCopyText(
   const fc = faceClaim ?? '';
 
   if (!lastPoints) {
-    return `<w>@"${username}"</w> › <i>${fc}</i>
-    <x>n'a jamais rp</x>`;
+    return `<w>@"${username}"</w> › <i>${fc}</i><br><x>n'a jamais rp</x>`;
   }
 
-  return `<w>@"${username}"</w> › <i>${fc}</i>
-  <x>n'a pas rp depuis le ${lastChangeAtDisplay}</x>`;
+  return `<w>@"${username}"</w> › <i>${fc}</i><br><x>n'a pas rp depuis le ${lastChangeAtDisplay}</x>`;
 }
